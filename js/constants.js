223b5f6d4cc1a4f4fa7aabdd51e32b17a71f62bc
Coder R <117482389+Isac-Coder@users.noreply.github.com>
2026-06-19 12:07:29 -0500

    V 0.1.4

--- js/constants.js
+++ js/constants.js
@@ -5,5 +5,5 @@ export const YTMUSIC_SERVER_URL = 'http://127.0.0.1:5000/ytlyrics';
 export const SEARCH_STORAGE_URL = 'http://127.0.0.1:3001/searches';
 export const SUGGESTION_LIMIT = 8;
 export const REQUEST_TIMEOUT_MS = 10000;
-export const CACHE_TTL_MS = 5 * 60 * 1000; // 5 minutos
+export const CACHE_TTL_MS = 5 * 60 * 1000; // 5 minutos 
 export const MAX_LYRIC_CANDIDATES = 5;
